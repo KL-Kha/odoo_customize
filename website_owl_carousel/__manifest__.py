89329af2de3eb7f94dc7596ab6caf7daa3383193
KL-Kha <baokha0009@gmail.com>
2023-08-06 22:48:30 +0700

    owl_carousel

---
+++ website_owl_carousel/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name' 'OWL Carousel',
+    'category' 'website',
+    'summary' 'Improve website with this amazing carousel',
+    'version' '16.0.0.0.1',
+    'description' 
+    ,
+    'author' 'Trey (www.trey.es)',
+    'license' 'AGPL-3',
+    'depends' [
+        'website',
+    ],
+    'data' [
+    ],
+    'assets': {'web.assets_backend':
+				   ['website_owl_carousel/static/lib/owl-carousel/owl.carousel.css',
+					'website_owl_carousel/static/lib/owl-carousel/owl.theme.css',
+					'website_owl_carousel/static/lib/owl-carousel/owl.carousel.min.js',
+					],
+			   },
+    'installable' True,
+    'application' False,
+    'auto_install' False,
+}
