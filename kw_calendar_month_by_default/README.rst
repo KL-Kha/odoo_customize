b5aa52005dd165fe9291f10ed43b397a49a34357
KL-Kha <baokha0009@gmail.com>
2023-08-10 22:39:44 +0700

    calendar_month

---
+++ kw_calendar_month_by_default/README.rst
@@ -0,0 +1,6 @@
+Calendar month view by default
+==============================
+
+Make month view default calendar view.
+
+This module is developed by the `KitWorks <https://kitworks.systems/>`__.
---
+++ kw_calendar_month_by_default/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Calendar month view by default',
+
+    'author': 'Kitworks Systems',
+    'website': 'https://kitworks.systems/',
+
+    'category': 'Productivity',
+    'license': 'OPL-1',
+    'version': '16.0.1.0.2',
+    'depends': [
+        'calendar',
+    ],
+    'data': [
+        'views/calendar_views.xml',
+    ],
+    'installable': True,
+
+    'images': [
+        'static/description/cover.png',
+        'static/description/icon.png',
+    ],
+}
